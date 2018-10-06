90ab86c9980efc33f6975b166d0c8683c27ccf71
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2018-10-06 19:24:20 +0300

    Fixing browser tests.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {Client} from "./common/dataspace/Client";
+export {ClusterClient} from "./common/dataspace/ClusterClient";
 export {Encode} from "./common/dataspace/Encode";
 export {Decode} from "./common/dataspace/Decode";

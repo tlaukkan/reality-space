ce25b08e23335a2f5ecc08d302460ef4249f25af
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2018-11-17 18:21:54 +0200

    Added basic id token creation and validation methods.

---
+++ src/node/util/jwt.ts
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+
+export function createIdToken(issuer: String, userId: string, userEmail: string, userName: string, privateKeyEncoded: string) {
+    let privateKey: string;
+    if (privateKeyEncoded.startsWith('-')) {
+        privateKey = privateKeyEncoded;
+    } else {
+        privateKey = Buffer.from(privateKeyEncoded, 'base64').toString();
+    }
+
+    var token = jwt.sign({
+        iss: issuer,
+        id: userId,
+        name: userName,
+        email: userEmail,
+        exp: Math.floor(Date.now() / 1000) + (60 * 60),
+    }, {
+        key: privateKey,
+        passphrase: 'top secret'
+    }, {
+        algorithm: 'RS256'
+    });
+    return token;
+}
+
+export function validateIdToken(idToken: string, publicKeyEncoded: string) : Map<String, String> {
+    let publicKey: string;
+    if (publicKeyEncoded.startsWith('-')) {
+        publicKey = publicKeyEncoded;
+    } else {
+        publicKey = Buffer.from(publicKeyEncoded, 'base64').toString();
+    }
+
+    var decoded = jwt.verify(idToken, publicKey, { algorithm: 'RS256'});
+    const map = new Map<String, String>();
+    Object.keys(decoded).forEach(key => {
+        map.set(key, decoded[key]);
+    });
+    return map;
+}

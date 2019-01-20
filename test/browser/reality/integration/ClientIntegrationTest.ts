25e130b570595f6a726ab40eadb32f442c23a770
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2019-01-20 08:12:30 +0200

    Revert "Fixing CORS."
    This reverts commit b8533fb7

--- test/browser/reality/integration/ClientIntegrationTest.ts
+++ test/browser/reality/integration/ClientIntegrationTest.ts
@@ -8,7 +8,7 @@ import {createTestIdToken} from "../../browser";
 
 describe('Integration Test Single Client', () => {
 
-    it('Should connect client to aframe-dataspace-0-0-0.herokuapp.com.', async () => {
+    it('Should connect client to public test cluster 0-0-0 processor.', async () => {
         const client = new RealityClient(DEFAULT_DIMENSION, PUBLIC_TEST_CLUSTER_PROCESSOR_0_0_0_NAME, PUBLIC_TEST_CLUSTER_PROCESSOR_0_0_0_URL, PUBLIC_TEST_CLUSTER_STORAGE_URL, PUBLIC_TEST_CLUSTER_CDN_URL, createTestIdToken());
         await client.connect();
         client.close();
--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,8 @@
 export const PUBLIC_TEST_CLUSTER_CONFIG_URL = "https://cdn.jsdelivr.net/gh/tlaukkan/reality-space@0.0.25/config/public-test-cluster.json";
 
 export const PUBLIC_TEST_CLUSTER_PROCESSOR_URL = "wss://rs-test-processor.herokuapp.com/";
-export const PUBLIC_TEST_CLUSTER_CDN_URL = "https://rs-test-storage.herokuapp.com/api/";
 export const PUBLIC_TEST_CLUSTER_STORAGE_URL = "https://rs-test-storage.herokuapp.com/api/";
+export const PUBLIC_TEST_CLUSTER_CDN_URL = "https://rs-test-storage.herokuapp.com/api/";
 
 export const PUBLIC_TEST_CLUSTER_PROCESSOR_0_0_0_URL = "wss://rs-test-processor-0-0-0.herokuapp.com/";
 

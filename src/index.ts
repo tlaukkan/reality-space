a19d6a48189449b196630a53344f2a07017b54c9
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2019-02-01 10:35:14 +0200

    Added log methods to exports.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export {RealityClient} from "./common/reality/RealityClient";
 export {ClusterClient} from "./common/reality/ClusterClient";
 export {Encode} from "./common/reality/Encode";
 export {Decode} from "./common/reality/Decode";
+export * from "./node/util/log";
+

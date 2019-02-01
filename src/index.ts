5bdd90c8520973f88611e52f13969d8c2e6d674b
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2019-02-01 11:22:40 +0200

    Reverted log methods to exports.

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export {RealityClient} from "./common/reality/RealityClient";
 export {ClusterClient} from "./common/reality/ClusterClient";
 export {Encode} from "./common/reality/Encode";
 export {Decode} from "./common/reality/Decode";
+export {IdTokenIssuer, getClusterConfiguration} from "./common/reality/Configuration";
 export * from "./node/util/log";
 
